9ab09fd2e12c3c1a020982350a4dd991a39bd018
vnhk05 <155543341+vnhk05@users.noreply.github.com>
2024-06-02 22:20:49 +0530

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch (op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            if (num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            else
+                cout << "Error! Division by zero is not allowed! ";
+            break;
+
+        default:
+            cout << "Error! operator is incorrect";
+            break;
+    }
+
+    return 0;
+}
